09e2beaa82ed26c0daf3db92739772ce9580fee1
Victoria Earl <kitsuta@gmail.com>
2023-08-30 17:26:08 -0400

    Disable COVID model checks

--- covid/model_checks.py
+++ covid/model_checks.py
@@ -4,18 +4,18 @@ from uber.model_checks import prereg_validation, validation, ignore_unassigned_a
 
 @prereg_validation.Attendee
 def read_covid_policy(attendee):
-    if not attendee.agreed_to_covid_policies:
+    if False and not attendee.agreed_to_covid_policies:
         return "Please read and agree to the COVID Policies for {}.".format(c.EVENT_NAME_AND_YEAR)
 
 
 @validation.Attendee
 def allowed_to_register(attendee):
-    if not attendee.age_group_conf['can_register']:
+    if False and not attendee.age_group_conf['can_register']:
         return 'Per our COVID policies, attendees {} years of age currently may not register. \
                 Please email regsupport@magfest.org for more info.'.format(attendee.age_group_conf['desc'].lower())
 
 
 @validation.Attendee
 def checked_in_covid_ready(attendee):
-    if attendee.checked_in and not attendee.covid_ready:
+    if False and attendee.checked_in and not attendee.covid_ready:
         return "Attendees must be marked as eligible to attend under our COVID policies before they can be checked in."
